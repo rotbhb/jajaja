4a713b5e8b359f9e8f38c1849804f34c32e7618f
rotbhb <Ky0ko_Tosh1n0@proton.me>
2024-08-09 09:00:42 -0600

    message

---
+++ lua/config/init.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true 
+vim.opt.autoindent = true 
+
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.swapfile = false
